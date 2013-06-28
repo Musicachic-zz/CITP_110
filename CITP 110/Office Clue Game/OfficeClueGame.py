a18dbf62ac26b9aaae314912920da11a2e0c2dee
Musicachic <Musicachic@gmail.com>
2013-06-27 23:55:51 -0400

    Doing a office clue game.

--- CITP 110/Office Clue Game/OfficeClueGame.py
+++ CITP 110/Office Clue Game/OfficeClueGame.py
@@ -10,10 +10,13 @@ def main():
     print("This is a office clue game.")
     People = find_people()
     print_people(People)
+    print("")
     Weapons = find_weapons()
     print_weapons(Weapons)
+    print("")
     Room = find_room()
     print_room(Room)
+    print("")
     Num_People = number_people(People)
     Computer_Person_Choice = get_computers_choice_person(Num_People)
     Num_Weapons = number_weapons(Weapons)
@@ -94,41 +97,41 @@ def print_room(Room):
         print(room)
 
 def number_people(People):
-    people_size = len(People)
+    people_size = len(People)-1
     return people_size
 
 def number_weapons(Weapons):
-    weapons_size = len(Weapons)
+    weapons_size = len(Weapons)-1
     return weapons_size
 
 def number_room(Room):
-    room_size = len(Room)
+    room_size = len(Room)-1
     return room_size
 
 def get_computers_choice_person(Num_People):
     x = Num_People
-    computer_choice = random.randint(0,x-1)
+    computer_choice = random.randint(0,x)
     return computer_choice
 
 def get_computers_choice_weapons(Num_Weapons):
     x = Num_Weapons
-    computer_choice = random.randint(0,x-1)
+    computer_choice = random.randint(0,x)
     return computer_choice
 
 def get_computers_choice_room(Num_Room):
     x = Num_Room
-    computer_choice = random.randint(0,x-1)
+    computer_choice = random.randint(0,x)
     return computer_choice
 
 def players_choice_people(People,Num_People):
     x = int(Num_People)
 
     choice = int(input("Enter a person you think got pwned "))
-    if choice < 0 or choice >= x:
-        print("Error: Please input a integer between 0 and", Num_People-1)
+    if choice < 0 or choice > x:
+        print("Error: Please input a integer between 0 and", Num_People)
         return players_choice_people(People,Num_People)
     else:
-        print(People[choice])
+        #print(People[choice])
         return choice
 
 
@@ -136,34 +139,33 @@ def players_choice_weapon(Weapons,Num_Weapons):
     x = int(Num_Weapons)
     choice = int(input("Enter a weapon you think was used to pwn someone "))
 
-    if choice < 0 or choice >= x:
-        print("Error: Please input a integer between 0 and", Num_Weapons-1)
+    if choice < 0 or choice > x:
+        print("Error: Please input a integer between 0 and", Num_Weapons)
         return players_choice_weapon(Weapons,Num_Weapons)
     else:
-        print(Weapons[choice])
+        #print(Weapons[choice])
         return choice
 
 def player_choice_room(Room,Num_Room):
     x = int(Num_Room)
     choice = int(input("Enter a room you think someone got pwned in "))
 
-    if choice < 0 or choice >=x:
-        print("Error: Please input a integer between 0 and", Num_Room-1)
+    if choice < 0 or choice >x:
+        print("Error: Please input a integer between 0 and", Num_Room)
         return player_choice_room(Room,Num_Room)
     else:
-        print(Room[choice])
+        #print(Room[choice])
         return choice
 
 def determine_the_meanie(People,Num_People,Computer_Person_Choice):
     while True:
 
         Players_Choice_Person = players_choice_people(People,Num_People)
-        print(Computer_Person_Choice)
         if Computer_Person_Choice == Players_Choice_Person:
-            print(People[Players_Choice_Person], "totally did it. Now what did they do it with?")
+            print(People[Players_Choice_Person], "totally got pwned. Now with what?")
             return People[Players_Choice_Person]
         else:
-            print(People[Players_Choice_Person], "didn't do it. Keep guessing.")
+            print(People[Players_Choice_Person], "didn't get pwned. Keep guessing.")
 
 def determine_the_weapon(People, Num_People, Computer_Person_Choice, Weapons,Num_Weapons,Computer_Weapon_Choice, Meanie):
     while True:
